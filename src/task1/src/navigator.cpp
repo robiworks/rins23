3ae488b3711d3c6ff45e72590064aa29fd7dba62
Robert Samardžija <rs1278@student.uni-lj.si>
2023-04-02 19:35:24 +0200

    Init navigator

---
+++ src/task1/src/navigator.cpp
@@ -0,0 +1,21 @@
+#include "ros/ros.h"
+
+#include <actionlib_msgs/GoalStatusArray.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Quaternion.h>
+#include <nav_msgs/GetMap.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <stdlib.h>
+#include <tf2/transform_datatypes.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_ros/transform_listener.h>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv) {
+  cout << "Not implemented yet\n";
+
+  return 0;
+}
