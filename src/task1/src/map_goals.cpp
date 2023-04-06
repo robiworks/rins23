50d3891d5123a9fa010a2a9759948232f3bbe000
Robert Samardžija <rs1278@student.uni-lj.si>
2023-04-06 09:50:16 +0200

    Update points

--- src/task1/src/map_goals.cpp
+++ src/task1/src/map_goals.cpp
@@ -35,6 +35,7 @@ sound_play::SoundClient*    sc         = NULL;
 struct TransformedPoint {
     float x;
     float y;
+    bool  rotate;
 };
 
 bool             map_ready            = false;
@@ -118,12 +119,12 @@ void rotate360() {
   geometry_msgs::Twist msg;
 
   // Set parameters for 360 degree rotation
-  msg.linear.x  = 0;
-  msg.angular.z = 1.30;
+  msg.linear.x  = 0.0;
+  msg.angular.z = 0.85;
 
   // Publish same message multiple times because else the robot will stop
   ROS_INFO("Rotating 360 degrees to observe environment");
-  for (int i = 0; i < 23; i++) {
+  for (int i = 0; i < 35; i++) {
     cmdvel_pub.publish(msg);
     rate.sleep();
   }
@@ -153,14 +154,16 @@ void statusCallback(const actionlib_msgs::GoalStatusArrayConstPtr &msg) {
       if (msg->status_list[i].status == actionlib_msgs::GoalStatus::SUCCEEDED && reachedGoal(ts)) {
         ROS_INFO("Navigation completed!");
         navigation_completed = true;
-        rotate360();
+        if (currentGoal.rotate)
+          rotate360();
         break;
       }
 
       if (msg->status_list[i].status == actionlib_msgs::GoalStatus::ABORTED) {
         ROS_WARN("Unable to reach goal!");
         navigation_completed = true;
-        rotate360();
+        if (currentGoal.rotate)
+          rotate360();
         break;
       }
     } catch (tf2::TransformException &ex) {
@@ -183,8 +186,9 @@ void navigateTo(TransformedPoint point) {
   ROS_INFO("Received navigate command, navigating to: (x: %f, y: %f)", point.x, point.y);
   goal_pub.publish(goal);
 
-  currentGoal.x = point.x;
-  currentGoal.y = point.y;
+  currentGoal.x      = point.x;
+  currentGoal.y      = point.y;
+  currentGoal.rotate = point.rotate;
 }
 
 void cancelCallback(const actionlib_msgs::GoalIDConstPtr &msg) {
@@ -192,16 +196,18 @@ void cancelCallback(const actionlib_msgs::GoalIDConstPtr &msg) {
   navigation_paused = true;
 
   ROS_INFO("Playing sound ...");
-  sc->say("Kosha Mona!", "voice_kal_diphone", 1.0);
-  ros::Duration(2.0).sleep();
+  sc->say("New face detected!", "voice_kal_diphone", 1.0);
+  ros::Duration(3.0).sleep();
 
   // Continue navigation and resend goal
   navigation_paused = false;
   navigateTo(currentGoal);
 }
 
+int INTEREST_POINTS_LENGTH = 11;
+
 void navigateThroughPoints(TransformedPoint* arr) {
-  for (int i = 0; i < 22; i++) {
+  for (int i = 0; i < INTEREST_POINTS_LENGTH; i++) {
     navigateTo(arr[i]);
 
     while (!navigation_completed) {
@@ -216,42 +222,58 @@ void navigateThroughPoints(TransformedPoint* arr) {
 
 int main(int argc, char** argv) {
   // Five hardcoded points
-  TransformedPoint hardcodedPoints[5];
-  hardcodedPoints[0].x = 0.2;
-  hardcodedPoints[0].y = -1.3;
-  hardcodedPoints[1].x = 3.2;
-  hardcodedPoints[1].y = -0.15;
-  hardcodedPoints[2].x = 1.65;
-  hardcodedPoints[2].y = 0.8;
-  hardcodedPoints[3].x = 0.6;
-  hardcodedPoints[3].y = 2.5;
-  hardcodedPoints[4].x = -0.65;
-  hardcodedPoints[4].y = 0.15;
+  // TransformedPoint hardcodedPoints[5];
+  // hardcodedPoints[0].x = 0.2;
+  // hardcodedPoints[0].y = -1.3;
+  // hardcodedPoints[1].x = 3.2;
+  // hardcodedPoints[1].y = -0.15;
+  // hardcodedPoints[2].x = 1.65;
+  // hardcodedPoints[2].y = 0.8;
+  // hardcodedPoints[3].x = 0.6;
+  // hardcodedPoints[3].y = 2.5;
+  // hardcodedPoints[4].x = -0.65;
+  // hardcodedPoints[4].y = 0.15;
+
+  // TransformedPoint interestPoints[] = {
+  //   { 0.00, -1.02},
+  //   { 1.13, -1.05},
+  //   { 2.25, -0.92},
+  //   { 3.65, -0.30},
+  //   { 3.08,  0.31},
+  //   { 2.29,  0.36},
+  //   { 1.19,  0.20},
+  //   { 1.10,  1.08},
+  //   { 2.01,  1.28},
+  //   { 2.40,  2.02},
+  //   { 2.04,  2.77},
+  //   { 1.14,  2.66},
+  //   { 1.02,  1.81}, // this one can be removed
+  //   { 0.24,  2.67},
+  //   {-0.50,  2.34},
+  //   {-1.06,  1.99},
+  //   {-1.65,  2.70},
+  //   {-0.78,  0.96},
+  //   {-1.33,  0.17},
+  //   {-0.57,  0.35}, // clean these points up
+  //   { 0.00,  0.97},
+  //   { 0.00,  0.17}
+  // };
 
   TransformedPoint interestPoints[] = {
-    { 0.00, -1.02},
-    { 1.13, -1.05},
-    { 2.25, -0.92},
-    { 3.65, -0.30},
-    { 3.08,  0.31},
-    { 2.29,  0.36},
-    { 1.19,  0.20},
-    { 1.10,  1.08},
-    { 2.01,  1.28},
-    { 2.40,  2.02},
-    { 2.04,  2.77},
-    { 1.14,  2.66},
-    { 1.02,  1.81},
-    { 0.24,  2.67},
-    {-0.50,  2.34},
-    {-1.06,  1.99},
-    {-1.65,  2.70},
-    {-0.78,  0.96},
-    {-1.33,  0.17},
-    {-0.57,  0.35},
-    { 0.00,  0.97},
-    { 0.00,  0.17}
+    {  -0.9712396264076233,  0.3039016127586365, true},
+    {    -0.81903076171875,  1.5590908527374268, true},
+    {  0.07623038440942764,   2.691239356994629, true},
+    {   2.1217899322509766,  2.7076127529144287, true}, // edit
+    {   2.5573792457580566,  1.3039171695709229, true},
+    {    1.286680817604065,  0.5794230103492737, true},
+    {    3.354393482208252, 0.27303266525268555, true}, // edit
+    {    3.716524362564087, -0.4115545153617859, true},
+    {   2.0537025928497314,  -1.007872462272644, true},
+    {0.0010448374086990952,  -1.046779990196228, true},
+    {  -0.1095728874206543, -1.1081676483154297, true}, // edit
   };
+
+  INTEREST_POINTS_LENGTH = 11;
 
   ros::init(argc, argv, "map_goals");
   ros::NodeHandle n;
@@ -271,8 +293,8 @@ int main(int argc, char** argv) {
   status_sub = n.subscribe("/move_base/status", 5, &statusCallback);
   cancel_sub = n.subscribe("/move_base/cancel", 2, &cancelCallback);
 
-  ROS_INFO("Waiting 15 seconds for other initializations");
-  ros::Duration(15.0).sleep();
+  ROS_INFO("Waiting for initialization");
+  ros::Duration(5.0).sleep();
   navigateThroughPoints(interestPoints);
 
   return 0;
