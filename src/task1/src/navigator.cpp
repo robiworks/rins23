c38589a186b66cb5000036e52ed443bcefcda000
Robert Samardžija <rs1278@student.uni-lj.si>
2023-04-10 14:05:42 +0200

    Add map initialization

---
+++ src/task1/src/navigator.cpp
@@ -0,0 +1,86 @@
+#include <geometry_msgs/TransformStamped.h>
+#include <nav_msgs/GetMap.h>
+#include <opencv2/core/core.hpp>
+#include <ros/ros.h>
+
+using namespace std;
+using namespace cv;
+
+/* ------------------------------------------------------------------------- */
+/*   Map initialization                                                      */
+/* ------------------------------------------------------------------------- */
+
+bool mapReady = false;
+
+void mapCallback(const nav_msgs::OccupancyGridConstPtr &msg_map) {
+  Mat                             cv_map;
+  float                           map_resolution = 0;
+  geometry_msgs::TransformStamped map_transform;
+
+  int size_x = msg_map->info.width;
+  int size_y = msg_map->info.height;
+
+  if ((size_x < 3) || (size_y < 3)) {
+    ROS_INFO(
+        "Map size is only x: %d,  y: %d . Not running map to image conversion",
+        size_x,
+        size_y
+    );
+    return;
+  }
+
+  // resize cv image if it doesn't have the same dimensions as the map
+  if ((cv_map.rows != size_y) && (cv_map.cols != size_x)) {
+    cv_map = cv::Mat(size_y, size_x, CV_8U);
+  }
+
+  map_resolution                        = msg_map->info.resolution;
+  map_transform.transform.translation.x = msg_map->info.origin.position.x;
+  map_transform.transform.translation.y = msg_map->info.origin.position.y;
+  map_transform.transform.translation.z = msg_map->info.origin.position.z;
+  map_transform.transform.rotation      = msg_map->info.origin.orientation;
+
+  const std::vector<int8_t> &map_msg_data(msg_map->data);
+  unsigned char*             cv_map_data = (unsigned char*) cv_map.data;
+
+  //We have to flip around the y axis, y for image starts at the top and y for map at the bottom
+  int size_y_rev = size_y - 1;
+
+  for (int y = size_y_rev; y >= 0; --y) {
+    int idx_map_y = size_x * (size_y - y);
+    int idx_img_y = size_x * y;
+
+    for (int x = 0; x < size_x; ++x) {
+      int idx = idx_img_y + x;
+
+      switch (map_msg_data[idx_map_y + x]) {
+        case -1:
+          cv_map_data[idx] = 127;
+          break;
+
+        case 0:
+          cv_map_data[idx] = 255;
+          break;
+
+        case 100:
+          cv_map_data[idx] = 0;
+          break;
+      }
+    }
+  }
+
+  ROS_INFO("Map is available");
+  mapReady = true;
+}
+
+int main(int argc, char* argv[]) {
+  // Initialize node
+  ros::init(argc, argv, "navigator");
+  ros::NodeHandle nh;
+
+  // Wait for map initialization
+  ros::Subscriber mapSub = nh.subscribe("map", 10, &mapCallback);
+  while (!mapReady) {
+    ros::spinOnce();
+  }
+}
