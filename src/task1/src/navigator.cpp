174a328c48b0683c59120cd7c2f77f06aec96091
Robert Samardžija <rs1278@student.uni-lj.si>
2023-04-10 15:53:20 +0200

    Navigator - navigation monitoring 1

--- src/task1/src/navigator.cpp
+++ src/task1/src/navigator.cpp
@@ -1,4 +1,5 @@
 #include <actionlib/client/simple_action_client.h>
+#include <actionlib/client/simple_client_goal_state.h>
 #include <geometry_msgs/TransformStamped.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <nav_msgs/GetMap.h>
@@ -51,7 +52,7 @@ class Navigator {
       ros::Duration(2.0).sleep();
 
       // Everything initialized, set current state to idle
-      currentState = IDLE;
+      currentState = NavigatorState::IDLE;
     }
 
     void navigateTo(NavigatorPoint point) {
@@ -67,12 +68,29 @@ class Navigator {
       goal.target_pose.pose.orientation.w = 1;
 
       ROS_INFO("[Navigator] Navigating to: (x: %.3f, y: %.3f)", point.x, point.y);
-      client->sendGoalAndWait(goal);
+      client->sendGoal(goal);
+
+      // Start monitoring navigation
+      monitorNavigation();
     }
 
   private:
     MoveBaseClient*          client;
     sound_play::SoundClient* soundClient;
+
+    void monitorNavigation() {
+      // Monitor navigation until it reaches a terminal state
+      actionlib::SimpleClientGoalState goalState = client->getState();
+      while (!goalState.isDone()) {
+        ROS_DEBUG("[Navigator] Current goal state: %s", goalState.toString().c_str());
+        ros::Duration(0.5).sleep();
+        goalState = client->getState();
+
+        // TODO Handle incoming messages to stop/explore etc.
+      }
+
+      // TODO Handle terminal states
+    }
 };
 
 /* ------------------------------------------------------------------------- */
@@ -153,18 +171,19 @@ int main(int argc, char* argv[]) {
     ros::spinOnce();
   }
 
-  NavigatorPoint interestPoints[] = {
+  // Vector of interest points in the map
+  vector<NavigatorPoint> interestPoints {
     {  -0.9712396264076233,  0.3039016127586365, true},
     {    -0.81903076171875,  1.5590908527374268, true},
     {  0.07623038440942764,   2.691239356994629, true},
-    {   2.1217899322509766,  2.7076127529144287, true}, // edit
+    {   2.1217899322509766,  2.7076127529144287, true},
     {   2.5573792457580566,  1.3039171695709229, true},
     {    1.286680817604065,  0.5794230103492737, true},
-    {    3.354393482208252, 0.27303266525268555, true}, // edit
+    {    3.354393482208252, 0.27303266525268555, true},
     {    3.716524362564087, -0.4115545153617859, true},
     {   2.0537025928497314,  -1.007872462272644, true},
     {0.0010448374086990952,  -1.046779990196228, true},
-    {  -0.1095728874206543, -1.1081676483154297, true}, // edit
+    {  -0.1095728874206543, -1.1081676483154297, true},
   };
 
   // Initialize Navigator
