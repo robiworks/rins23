83d160ef93a45b501144dba21cff1bed411ccb88
Gasper Spagnolo <gasper.spagnolo@outlook.com>
2023-04-29 21:10:37 +0200

    Cylnder detection works

--- src/task2/src/cylinder.cpp
+++ src/task2/src/cylinder.cpp
@@ -1,6 +1,11 @@
-#include <geometry_msgs/PointStamped.h>
+#include "geometry_msgs/PointStamped.h"
+#include "pcl/point_cloud.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include "tf2_ros/transform_listener.h"
+
+#include <iostream>
+#include <math.h>
 #include <pcl/ModelCoefficients.h>
-#include <pcl/common/centroid.h>
 #include <pcl/features/normal_3d.h>
 #include <pcl/filters/extract_indices.h>
 #include <pcl/filters/passthrough.h>
@@ -10,177 +15,198 @@
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
 #include <pcl_conversions/pcl_conversions.h>
-#include <pcl_ros/point_cloud.h>
-#include <pcl_ros/transforms.h>
 #include <ros/ros.h>
-#include <tf/transform_listener.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-#include <tf2_ros/transform_listener.h>
+#include <sensor_msgs/PointCloud2.h>
 #include <visualization_msgs/Marker.h>
+
+ros::Publisher pubx;
+ros::Publisher puby;
+ros::Publisher pubm;
+
+tf2_ros::Buffer tf2_buffer;
 
 typedef pcl::PointXYZ PointT;
 
-class CylinderDetection {
-  public:
-    CylinderDetection() : nh_("~"), tf2_listener(tf2_buffer) {
-      sub_ = nh_.subscribe("/camera/depth/points", 1, &CylinderDetection::cloudCallback, this);
-      pub_ = nh_.advertise<geometry_msgs::PointStamped>("cylinder_position", 1);
+void cloud_cb(const pcl::PCLPointCloud2ConstPtr &cloud_blob) {
+  // All the objects needed
+
+  ros::Time time_rec, time_test;
+  time_rec = ros::Time::now();
+
+  pcl::PassThrough<PointT>                             pass;
+  pcl::NormalEstimation<PointT, pcl::Normal>           ne;
+  pcl::SACSegmentationFromNormals<PointT, pcl::Normal> seg;
+  pcl::PCDWriter                                       writer;
+  pcl::ExtractIndices<PointT>                          extract;
+  pcl::ExtractIndices<pcl::Normal>                     extract_normals;
+  pcl::search::KdTree<PointT>::Ptr                     tree(new pcl::search::KdTree<PointT>());
+  Eigen::Vector4f                                      centroid;
+
+  // Datasets
+  pcl::PointCloud<PointT>::Ptr      cloud(new pcl::PointCloud<PointT>);
+  pcl::PointCloud<PointT>::Ptr      cloud_filtered(new pcl::PointCloud<PointT>);
+  pcl::PointCloud<pcl::Normal>::Ptr cloud_normals(new pcl::PointCloud<pcl::Normal>);
+  pcl::PointCloud<PointT>::Ptr      cloud_filtered2(new pcl::PointCloud<PointT>);
+  pcl::PointCloud<pcl::Normal>::Ptr cloud_normals2(new pcl::PointCloud<pcl::Normal>);
+  pcl::ModelCoefficients::Ptr       coefficients_plane(new pcl::ModelCoefficients),
+      coefficients_cylinder(new pcl::ModelCoefficients);
+  pcl::PointIndices::Ptr inliers_plane(new pcl::PointIndices),
+      inliers_cylinder(new pcl::PointIndices);
+
+  // Read in the cloud data
+  pcl::fromPCLPointCloud2(*cloud_blob, *cloud);
+
+  // Build a passthrough filter to remove spurious NaNs
+  pass.setInputCloud(cloud);
+  pass.setFilterFieldName("z");
+  pass.setFilterLimits(0, 1.5);
+  pass.filter(*cloud_filtered);
+
+  // Estimate point normals
+  ne.setSearchMethod(tree);
+  ne.setInputCloud(cloud_filtered);
+  ne.setKSearch(50);
+  ne.compute(*cloud_normals);
+
+  // Create the segmentation object for the planar model and set all the parameters
+  seg.setOptimizeCoefficients(true);
+  seg.setModelType(pcl::SACMODEL_NORMAL_PLANE);
+  seg.setNormalDistanceWeight(0.1);
+  seg.setMethodType(pcl::SAC_RANSAC);
+  seg.setMaxIterations(100);
+  seg.setDistanceThreshold(0.03);
+  seg.setInputCloud(cloud_filtered);
+  seg.setInputNormals(cloud_normals);
+  // Obtain the plane inliers and coefficients
+  seg.segment(*inliers_plane, *coefficients_plane);
+
+  // Extract the planar inliers from the input cloud
+  extract.setInputCloud(cloud_filtered);
+  extract.setIndices(inliers_plane);
+  extract.setNegative(false);
+
+  // Write the planar inliers to disk
+  pcl::PointCloud<PointT>::Ptr cloud_plane(new pcl::PointCloud<PointT>());
+  extract.filter(*cloud_plane);
+
+  pcl::PCLPointCloud2 outcloud_plane;
+  pcl::toPCLPointCloud2(*cloud_plane, outcloud_plane);
+  pubx.publish(outcloud_plane);
+
+  // Remove the planar inliers, extract the rest
+  extract.setNegative(true);
+  extract.filter(*cloud_filtered2);
+  extract_normals.setNegative(true);
+  extract_normals.setInputCloud(cloud_normals);
+  extract_normals.setIndices(inliers_plane);
+  extract_normals.filter(*cloud_normals2);
+
+  // Create the segmentation object for cylinder segmentation and set all the parameters
+  seg.setOptimizeCoefficients(true);
+  seg.setModelType(pcl::SACMODEL_CYLINDER);
+  seg.setMethodType(pcl::SAC_RANSAC);
+  seg.setNormalDistanceWeight(0.1);
+  seg.setMaxIterations(10000);
+  seg.setDistanceThreshold(0.05);
+  seg.setRadiusLimits(0.06, 0.2);
+  seg.setInputCloud(cloud_filtered2);
+  seg.setInputNormals(cloud_normals2);
+
+  // Obtain the cylinder inliers and coefficients
+  seg.segment(*inliers_cylinder, *coefficients_cylinder);
+
+  // Write the cylinder inliers to disk
+  extract.setInputCloud(cloud_filtered2);
+  extract.setIndices(inliers_cylinder);
+  extract.setNegative(false);
+  pcl::PointCloud<PointT>::Ptr cloud_cylinder(new pcl::PointCloud<PointT>());
+  extract.filter(*cloud_cylinder);
+  if (cloud_cylinder->points.empty())
+    std::cerr << "Can't find the cylindrical component." << std::endl;
+  else {
+    pcl::compute3DCentroid(*cloud_cylinder, centroid);
+    std::cerr << "centroid of the cylindrical component: " << centroid[0] << " " << centroid[1]
+              << " " << centroid[2] << " " << centroid[3] << std::endl;
+
+    geometry_msgs::PointStamped     point_camera;
+    geometry_msgs::PointStamped     point_map;
+    visualization_msgs::Marker      marker;
+    geometry_msgs::TransformStamped tss;
+
+    point_camera.header.frame_id = "camera_rgb_optical_frame";
+    point_camera.header.stamp    = ros::Time::now();
+
+    point_map.header.frame_id = "map";
+    point_map.header.stamp    = ros::Time::now();
+
+    point_camera.point.x = centroid[0];
+    point_camera.point.y = centroid[1];
+    point_camera.point.z = centroid[2];
+
+    try {
+      time_test = ros::Time::now();
+      tss       = tf2_buffer.lookupTransform("map", "camera_rgb_optical_frame", time_rec);
+    } catch (tf2::TransformException &ex) {
+      ROS_WARN("Transform warning: %s\n", ex.what());
     }
+    tf2::doTransform(point_camera, point_map, tss);
 
-    void cloudCallback(const sensor_msgs::PointCloud2::ConstPtr& cloud_msg) {
-        pcl::PointCloud<PointT>::Ptr cloud(new pcl::PointCloud<PointT>);
-        pcl::fromROSMsg(*cloud_msg, *cloud);
+    std::cerr << "point_map: " << point_map.point.x << " " << point_map.point.y << " "
+              << point_map.point.z << std::endl;
 
-      // get the timestamp from the point cloud
-      ros::Time time_rec = cloud_msg->header.stamp;
+    marker.header.frame_id = "map";
+    marker.header.stamp    = ros::Time::now();
 
-      // All the objects needed
-      pcl::PassThrough<PointT>                             pass;
-      pcl::NormalEstimation<PointT, pcl::Normal>           ne;
-      pcl::SACSegmentationFromNormals<PointT, pcl::Normal> seg;
-      pcl::ExtractIndices<PointT>                          extract;
-      pcl::ExtractIndices<pcl::Normal>                     extract_normals;
-      pcl::search::KdTree<PointT>::Ptr                     tree(new pcl::search::KdTree<PointT>());
+    marker.ns = "cylinder";
+    marker.id = 0;
 
-      // Datasets
-      pcl::PointCloud<PointT>::Ptr      cloud_filtered(new pcl::PointCloud<PointT>);
-      pcl::PointCloud<pcl::Normal>::Ptr cloud_normals(new pcl::PointCloud<pcl::Normal>);
-      pcl::PointCloud<PointT>::Ptr      cloud_filtered2(new pcl::PointCloud<PointT>);
-      pcl::PointCloud<pcl::Normal>::Ptr cloud_normals2(new pcl::PointCloud<pcl::Normal>);
-      pcl::ModelCoefficients::Ptr       coefficients_plane(new pcl::ModelCoefficients),
-          coefficients_cylinder(new pcl::ModelCoefficients);
-      pcl::PointIndices::Ptr inliers_plane(new pcl::PointIndices),
-          inliers_cylinder(new pcl::PointIndices);
+    marker.type   = visualization_msgs::Marker::CYLINDER;
+    marker.action = visualization_msgs::Marker::ADD;
 
-      // Build a passthrough filter to remove spurious NaNs
-      pass.setInputCloud(cloud);
-      pass.setFilterFieldName("z");
-      pass.setFilterLimits(0, 1.5);
-      pass.filter(*cloud_filtered);
-      ROS_INFO_STREAM(
-          "PointCloud after filtering has: " << cloud_filtered->size() << " data points."
-      );
+    marker.pose.position.x    = point_map.point.x;
+    marker.pose.position.y    = point_map.point.y;
+    marker.pose.position.z    = point_map.point.z;
+    marker.pose.orientation.x = 0.0;
+    marker.pose.orientation.y = 0.0;
+    marker.pose.orientation.z = 0.0;
+    marker.pose.orientation.w = 1.0;
 
-      // Estimate point normals
-      ne.setSearchMethod(tree);
-      ne.setInputCloud(cloud_filtered);
-      ne.setKSearch(50);
-      ne.compute(*cloud_normals);
+    marker.scale.x = 0.1;
+    marker.scale.y = 0.1;
+    marker.scale.z = 0.1;
 
-      // Create the segmentation object for the planar model and set all the parameters
-      seg.setOptimizeCoefficients(true);
-      seg.setModelType(pcl::SACMODEL_NORMAL_PLANE);
-      seg.setNormalDistanceWeight(0.1);
-      seg.setMethodType(pcl::SAC_RANSAC);
-      seg.setMaxIterations(100);
-      seg.setDistanceThreshold(0.03);
-      seg.setInputCloud(cloud_filtered);
-      seg.setInputNormals(cloud_normals);
-      // Obtain the plane inliers and coefficients
-      seg.segment(*inliers_plane, *coefficients_plane);
-      //ROS_INFO_STREAM("Plane coefficients: " << *coefficients_plane);
+    marker.color.r = 0.0f;
+    marker.color.g = 1.0f;
+    marker.color.b = 0.0f;
+    marker.color.a = 1.0f;
 
-      // Extract the planar inliers from the input cloud
-      extract.setInputCloud(cloud_filtered);
-      extract.setIndices(inliers_plane);
-      extract.setNegative(false);
+    marker.lifetime = ros::Duration();
 
-      // Remove the planar inliers, extract the rest
-      extract.setNegative(true);
-      extract.filter(*cloud_filtered2);
-      extract_normals.setNegative(true);
-      extract_normals.setInputCloud(cloud_normals);
-      extract_normals.setIndices(inliers_plane);
-      extract_normals.filter(*cloud_normals2);
+    pubm.publish(marker);
 
-      // Check if there are any points left
-      if (cloud_filtered2->points.empty() || cloud_normals2->points.empty()) {
-        ROS_WARN("Could not find any points remaining after planar extraction.");
-        return;
-      }
-
-      // Create the segmentation object for cylinder segmentation and set all the parameters
-      seg.setOptimizeCoefficients(true);
-      seg.setModelType(pcl::SACMODEL_CYLINDER);
-      seg.setMethodType(pcl::SAC_RANSAC);
-      seg.setNormalDistanceWeight(0.1);
-      seg.setMaxIterations(10000);
-      seg.setDistanceThreshold(0.05);
-      seg.setRadiusLimits(0.06, 0.2);
-      seg.setInputCloud(cloud_filtered2);
-      seg.setInputNormals(cloud_normals2);
-
-      // Obtain the cylinder inliers and coefficients
-      seg.segment(*inliers_cylinder, *coefficients_cylinder);
-
-      extract.setInputCloud(cloud_filtered2);
-      extract.setIndices(inliers_cylinder);
-      extract.setNegative(false);
-      pcl::PointCloud<PointT>::Ptr cloud_cylinder(new pcl::PointCloud<PointT>());
-      extract.filter(*cloud_cylinder);
-      if (cloud_cylinder->points.empty() == 0) {
-        ROS_WARN("No cylinder found");
-        return;
-      } else {
-        // Get the centroid of the cylindrical component
-        pcl::compute3DCentroid(*cloud_cylinder, centroid);
-
-        // Transform the centroid from the camera frame to the map frame
-        geometry_msgs::PointStamped point_camera, point_map;
-        point_camera.header.frame_id = "camera_rgb_optical_frame";
-        point_camera.header.stamp    = ros::Time::now();
-        point_map.header.frame_id    = "map";
-        point_map.header.stamp       = ros::Time::now();
-
-        point_camera.point.x = centroid[0];
-        point_camera.point.y = centroid[1];
-        point_camera.point.z = centroid[2];
-
-        try {
-          ros::Time                       time_test = ros::Time::now();
-          geometry_msgs::TransformStamped tss =
-              tf2_buffer.lookupTransform("map", "camera_rgb_optical_frame", time_rec);
-          tf2::doTransform(point_camera, point_map, tss);
-        } catch (tf2::ransformException &ex) {
-          ROS_WARN("Transform warning: %s\n", ex.what());
-        }
-
-        // Create a marker for visualization
-        visualization_msgs::Marker marker;
-        marker.header.frame_id = "map";
-        marker.header.stamp    = ros::Time::now();
-        marker.ns              = "cylinder";
-        marker.id              = 0;
-        marker.type            = visualization_msgs::Marker::CYLINDER;
-        marker.action          = visualization_msgs::Marker::ADD;
-
-        ROS_ERROR("Centroid: %f, %f, %f", point_map.point.x, point_map.point.y, point_map.point.z);
-
-        marker.pose.position.x    = point_map.point.x;
-        marker.pose.position.y    = point_map.point.y;
-        marker.pose.position.z    = point_map.point.z;
-        marker.pose.orientation.x = 0.0;
-        marker.pose.orientation.y = 0.0;
-        marker.pose.orientation.z = 0.0;
-        marker.pose.orientation.w = 1.0;
-      }
-    }
-
-  private:
-    ros::NodeHandle            nh_;
-    ros::Subscriber            sub_;
-    ros::Publisher             pub_;
-    ros::Publisher             pubm;
-    ros::Publisher             puby;
-    pcl::PCDWriter             writer;
-    tf2_ros::Buffer            tf2_buffer;
-    tf2_ros::TransformListener tf2_listener;
-    Eigen::Vector4f            centroid;
-};
+    pcl::PCLPointCloud2 outcloud_cylinder;
+    pcl::toPCLPointCloud2(*cloud_cylinder, outcloud_cylinder);
+    puby.publish(outcloud_cylinder);
+  }
+}
 
 int main(int argc, char** argv) {
-  ros::init(argc, argv, "cylinder_detection_node");
-  CylinderDetection cylinder_detection;
+  // Initialize ROS
+  ros::init(argc, argv, "cylinder_segment");
+  ros::NodeHandle nh;
+
+  // For transforming between coordinate frames
+  tf2_ros::TransformListener tf2_listener(tf2_buffer);
+
+  // Create a ROS subscriber for the input point cloud
+  ros::Subscriber sub = nh.subscribe("/camera/depth/points", 1, cloud_cb);
+
+  // Create a ROS publisher for the output point cloud
+  pubx = nh.advertise<pcl::PCLPointCloud2>("/custom_msgs", 1);
+  puby = nh.advertise<pcl::PCLPointCloud2>("cylinder", 1);
+
+  pubm = nh.advertise<visualization_msgs::Marker>("detected_cylinder", 1);
+
+  // Spin
   ros::spin();
-  return 0;
 }
