3f46fae50ae3d172c9bcb785f605fc3e464fdaab
nyanSpruk <nik.janspruk@gmail.com>
2023-04-29 15:15:25 +0200

    Separated circles and ground circle launch file, calculates the mean point and sends that

--- src/task2/src/ground_ring.cpp
+++ src/task2/src/ground_ring.cpp
@@ -8,10 +8,10 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
 #include <task2/RingPoseMsg.h>
-#include <visualization_msgs/Marker.h>
-#include <visualization_msgs/MarkerArray.h>
 #include <trajectory_msgs/JointTrajectory.h>
 #include <trajectory_msgs/JointTrajectoryPoint.h>
+#include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
 
 using namespace message_filters;
 using namespace sensor_msgs;
@@ -19,8 +19,11 @@ using namespace cv_bridge;
 
 typedef sync_policies::ApproximateTime<Image, Image> ApproxSync;
 
-visualization_msgs::MarkerArray    marker_array;
+visualization_msgs::MarkerArray marker_array;
 
+// Array of detections
+std::vector<geometry_msgs::PointStamped> detections;
+int                             detection_count = 0;
 
 bool debug;
 bool search = false;
@@ -97,7 +100,7 @@ void getDepths(
 
     // Debug the color
     if (debug) {
-      ROS_INFO("Color: %f, %f, %f", pose.color.r, pose.color.g, pose.color.b);
+      // ROS_INFO("Color: %f, %f, %f", pose.color.r, pose.color.g, pose.color.b);
     }
 
     // Calculate the distance with angles and the depth
@@ -116,48 +119,94 @@ void getDepths(
     point.point.y         = 0;
     point.point.z         = x_target; //Try switching .y and .z
 
-    // Make a marker for the point
-    visualization_msgs::Marker marker;
-    marker.header.frame_id    = "camera_rgb_optical_frame";
-    marker.header.stamp       = depth_header.stamp;
-    marker.ns                 = "points_and_lines";
-    marker.id                 = rand();
-    marker.type               = visualization_msgs::Marker::SPHERE;
-    marker.action             = visualization_msgs::Marker::ADD;
-    marker.pose.position.x    = point.point.x;
-    marker.pose.position.y    = point.point.y;    
-    marker.pose.position.z    = point.point.z;
-    marker.pose.orientation.x = 0.0;
-    marker.pose.orientation.y = 0.0;
-    marker.pose.orientation.z = 0.0;
-    marker.pose.orientation.w = 1.0;
-    marker.color.r            = pose.color.r / 255;
-    marker.color.g            = pose.color.g / 255;
-    marker.color.b            = pose.color.b / 255;
-    marker.color.a            = 1.0;
-    marker.scale.x            = 0.1;
-    marker.scale.y            = 0.1;
-    marker.scale.z            = 0.1;
-    marker.lifetime           = ros::Duration();
+    // // Make a marker for the point
+    // visualization_msgs::Marker marker;
+    // marker.header.frame_id    = "camera_rgb_optical_frame";
+    // marker.header.stamp       = depth_header.stamp;
+    // marker.ns                 = "points_and_lines";
+    // marker.id                 = rand();
+    // marker.type               = visualization_msgs::Marker::SPHERE;
+    // marker.action             = visualization_msgs::Marker::ADD;
+    // marker.pose.position.x    = point.point.x;
+    // marker.pose.position.y    = point.point.y;
+    // marker.pose.position.z    = point.point.z;
+    // marker.pose.orientation.x = 0.0;
+    // marker.pose.orientation.y = 0.0;
+    // marker.pose.orientation.z = 0.0;
+    // marker.pose.orientation.w = 1.0;
+    // marker.color.r            = pose.color.r / 255;
+    // marker.color.g            = pose.color.g / 255;
+    // marker.color.b            = pose.color.b / 255;
+    // marker.color.a            = 1.0;
+    // marker.scale.x            = 0.1;
+    // marker.scale.y            = 0.1;
+    // marker.scale.z            = 0.1;
+    // marker.lifetime           = ros::Duration();
 
-    marker_array.markers.push_back(marker);
-    
-    marker_pub.publish(marker_array);
+    // marker_array.markers.push_back(marker);
 
-    // Print point stamped
-    // ROS_INFO("Point: %f, %f, %f", point.point.x, point.point.y, point.point.z);
-
+    // marker_pub.publish(marker_array);
     geometry_msgs::Pose pose_msg;
 
     try {
-      pose_msg.position.x = point.point.x;
-      pose_msg.position.y = point.point.y;
-      pose_msg.position.z = point.point.z;
+      if (detection_count < 5) {
+        ROS_WARN("Not enough detections");
+        detections.push_back(point);
+        detection_count++;
+      } else {
+        // Calculate the mean
+        float mean_x = 0;
+        float mean_y = 0;
+        float mean_z = 0;
 
-      pose.pose = pose_msg;
+        for (size_t i = 0; i < detections.size(); i++) {
+          mean_x += detections[i].point.x;
+          mean_y += detections[i].point.y;
+          mean_z += detections[i].point.z;
+        }
 
-      ground_ring_pub.publish(pose);
-    //   search = false;
+        mean_x /= detections.size();
+        mean_y /= detections.size();
+        mean_z /= detections.size();
+
+        // Make a marker for the point
+        visualization_msgs::Marker marker;
+        marker.header.frame_id    = "camera_rgb_optical_frame";
+        marker.header.stamp       = depth_header.stamp;
+        marker.ns                 = "points_and_lines";
+        marker.id                 = rand();
+        marker.type               = visualization_msgs::Marker::SPHERE;
+        marker.action             = visualization_msgs::Marker::ADD;
+        marker.pose.position.x    = mean_x;
+        marker.pose.position.y    = mean_y;
+        marker.pose.position.z    = mean_z;
+        marker.pose.orientation.x = 0.0;
+        marker.pose.orientation.y = 0.0;
+        marker.pose.orientation.z = 0.0;
+        marker.pose.orientation.w = 1.0;
+        marker.color.r            = pose.color.r / 255;
+        marker.color.g            = pose.color.g / 255;
+        marker.color.b            = pose.color.b / 255;
+        marker.color.a            = 1.0;
+        marker.scale.x            = 0.1;
+        marker.scale.y            = 0.1;
+        marker.scale.z            = 0.1;
+        marker.lifetime           = ros::Duration();
+
+        marker_array.markers.push_back(marker);
+
+        marker_pub.publish(marker_array);
+
+        pose_msg.position.x = mean_x;
+        pose_msg.position.y = mean_y;
+        pose_msg.position.z = mean_z;
+
+        pose.pose = pose_msg;
+
+        ground_ring_pub.publish(pose);
+        search = false;
+      }
+
     } catch (const std::exception &e) {
       ROS_ERROR("Transform error: %s", e.what());
       continue;
@@ -165,10 +214,10 @@ void getDepths(
   }
 }
 
-std::vector<cv::Vec4f> detectCircles(cv::Mat input_img, cv::Mat output_img){
-    std::vector<cv::Vec4f> circles, validCircles;
+std::vector<cv::Vec4f> detectCircles(cv::Mat input_img, cv::Mat output_img) {
+  std::vector<cv::Vec4f> circles, validCircles;
 
-      // Arugments for hough transform
+  // Arugments for hough transform
   int   minRadius            = 160;
   int   maxRadius            = 300;
   int   minDist              = 100;
@@ -204,7 +253,7 @@ std::vector<cv::Vec4f> detectCircles(cv::Mat input_img, cv::Mat output_img){
 
     // Draw the circle outline
     if (debug) {
-      ROS_WARN("Circle radius: %d", radius);
+      // ROS_WARN("Circle radius: %d", radius);
 
       // Draw the center of the circle
       cv::circle(output_img, center, 3, cv::Scalar(0, 255, 0), -1);
@@ -221,9 +270,9 @@ void image_callback(
     const sensor_msgs::Image::ConstPtr &rgb_image,
     const sensor_msgs::Image::ConstPtr &depth_image
 ) {
-    if (!search)
-        return;
-    
+  if (!search)
+    return;
+
   cv_bridge::CvImageConstPtr cv_ptr;
   cv_bridge::CvImageConstPtr cv_rgb;
 
@@ -236,8 +285,8 @@ void image_callback(
   }
 
   // Convert the image to grayscale and to 8bit from 32bit
-        cv::Mat gray_img = cv::Mat(cv_rgb->image.rows, cv_rgb->image.cols, CV_8UC1);
-        cv::cvtColor(cv_rgb->image, gray_img, CV_BGR2GRAY);
+  cv::Mat gray_img = cv::Mat(cv_rgb->image.rows, cv_rgb->image.cols, CV_8UC1);
+  cv::cvtColor(cv_rgb->image, gray_img, CV_BGR2GRAY);
 
   // Same for rgb for showing the result
   cv::Mat rgb_img = cv::Mat(gray_img.size(), CV_8UC3);
@@ -249,35 +298,35 @@ void image_callback(
   getDepths(circles, cv_ptr, cv_rgb, rgb_img, depth_image->header);
 }
 
-void green_callback(task2::RingPoseMsg pose){
+void green_callback(task2::RingPoseMsg pose) {
   ROS_WARN("Green callback");
 
-    // Move the arm to the position
-    trajectory_msgs::JointTrajectory trajectory;
-    trajectory_msgs::JointTrajectoryPoint point;
+  // Move the arm to the position
+  trajectory_msgs::JointTrajectory      trajectory;
+  trajectory_msgs::JointTrajectoryPoint point;
 
-    trajectory.joint_names.push_back("arm_shoulder_pan_joint");
-    trajectory.joint_names.push_back("arm_shoulder_lift_joint");
-    trajectory.joint_names.push_back("arm_elbow_flex_joint");
-    trajectory.joint_names.push_back("arm_wrist_flex_joint");
+  trajectory.joint_names.push_back("arm_shoulder_pan_joint");
+  trajectory.joint_names.push_back("arm_shoulder_lift_joint");
+  trajectory.joint_names.push_back("arm_elbow_flex_joint");
+  trajectory.joint_names.push_back("arm_wrist_flex_joint");
 
-    point.positions.push_back(0);
-    point.positions.push_back(0.3);
-    point.positions.push_back(0.9);
-    point.positions.push_back(0);
+  point.positions.push_back(0);
+  point.positions.push_back(0.3);
+  point.positions.push_back(0.9);
+  point.positions.push_back(0);
 
-    point.time_from_start = ros::Duration(1.0);
-    
-    trajectory.points.push_back(point);
+  point.time_from_start = ros::Duration(1.0);
 
-    arm_pub.publish(trajectory);
-  
-    //wait 2 sec
-    ros::Duration(5.0).sleep();
+  trajectory.points.push_back(point);
+
+  arm_pub.publish(trajectory);
+
+  //wait 2 sec
+  ros::Duration(5.0).sleep();
   search = true;
 }
 
-void stop_green_callback(task2::RingPoseMsg pose){
+void stop_green_callback(task2::RingPoseMsg pose) {
   ROS_WARN("Stop green callback");
   search = false;
 }
@@ -308,24 +357,26 @@ int main(int argc, char** argv) {
   ROS_INFO("RGB topic: %s", rgb_topic.c_str());
   ROS_INFO("Debug: %s", debug ? "true" : "false");
 
-    marker_pub = nh.advertise<visualization_msgs::MarkerArray>("ground_ring_marker", 10000);
+  marker_pub = nh.advertise<visualization_msgs::MarkerArray>("ground_ring_marker", 10000);
 
-    ros::Subscriber green_sub = nh.subscribe("/custom_msgs/nav/green_ring_detected", 1, &green_callback);
+  ros::Subscriber green_sub =
+      nh.subscribe("/custom_msgs/nav/green_ring_detected", 1, &green_callback);
 
-    ground_ring_pub = nh.advertise<task2::RingPoseMsg>("/custom_msgs/ground_ring_detection", 1000);
+  ground_ring_pub = nh.advertise<task2::RingPoseMsg>("/custom_msgs/ground_ring_detection", 1000);
 
-    Subscriber<Image> rgb_sub(nh, rgb_topic, 1);
-    Subscriber<Image> depth_sub(nh, depth_topic, 1);
+  Subscriber<Image> rgb_sub(nh, rgb_topic, 1);
+  Subscriber<Image> depth_sub(nh, depth_topic, 1);
 
-      // Create a ApproximateTimeSynchronizer to synchronize the rgb and depth images
-    Synchronizer<ApproxSync> sync(ApproxSync(5), rgb_sub, depth_sub);
+  // Create a ApproximateTimeSynchronizer to synchronize the rgb and depth images
+  Synchronizer<ApproxSync> sync(ApproxSync(5), rgb_sub, depth_sub);
 
-    // Register a callback for the synchronizer. _1 and _2 are placeholders for the rgb and depth images
-    sync.registerCallback(boost::bind(&image_callback, _1, _2));
+  // Register a callback for the synchronizer. _1 and _2 are placeholders for the rgb and depth images
+  sync.registerCallback(boost::bind(&image_callback, _1, _2));
 
-    arm_pub = nh.advertise<trajectory_msgs::JointTrajectory>("/turtlebot_arm/arm_controller/command", 1000);
+  arm_pub =
+      nh.advertise<trajectory_msgs::JointTrajectory>("/turtlebot_arm/arm_controller/command", 1000);
 
-    ros::spin();
+  ros::spin();
 
-    return 0;
-  }
+  return 0;
+}
